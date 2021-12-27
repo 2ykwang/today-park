21db5a6200982d49c94d49e5d23c255e52ddd6cc
Yeonggwang <immutable000@gmail.com>
2021-12-27 18:59:55 +0900

    feat: user model 재정의
    AbstractBaseUser 상속받아 재정의

--- backend/apps/user/admin.py
+++ backend/apps/user/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+
+@admin.register(User)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "email",
+        "nickname",
+    ]
--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,16 +1,44 @@
 import os
 import uuid
 
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 
-# Create your models here.
+from .validators import NickNameValidator
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
     """
-    id, username, first_name, last_name, email, is_staff, is_active, date_joined(created)
+    username, nickname, email, profile_image
+
+    AbstractBaseUser
+        fields:
+            password, last_login, is_active
+    PermissionsMixin
+        fields:
+            is_superuser, groups, user_permissions
     """  # noqa
+
+    username_validator = UnicodeUsernameValidator()
+    nickname_validator = NickNameValidator()
+
+    username = models.CharField(
+        verbose_name="User name",
+        max_length=128,
+        unique=True,
+        validators=[username_validator],
+    )
+
+    # 닉네임 중복 가능한지..?
+    nickname = models.CharField(
+        verbose_name="user nickname",
+        max_length=30,
+        unique=True,
+        validators=[nickname_validator],
+    )
+    email = models.EmailField(verbose_name="User Email", max_length=128, unique=True)
+    is_staff = models.BooleanField(verbose_name="is staff", default=False)
 
     def _get_uuid_path(instance, filename):
         uuid4 = uuid.uuid4()
@@ -18,8 +46,13 @@ class User(AbstractUser):
         return new_path
 
     profile_image = models.ImageField(
-        verbose_name="user image", upload_to=_get_uuid_path
+        verbose_name="user image", upload_to=_get_uuid_path, blank=True
     )
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email", "nickname"]
 
     class Meta:
         db_table = "user"
