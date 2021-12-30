cee2ecb8ffd273bb5184d2909d2aa11e8234153b
Martina Lee <martinalee0528@gmail.com>
2021-12-30 15:11:45 +0900

    feat: equipment, parkequipment 모델 생성  #3
    equipment 모델 생성
    parkqeuipment 모델 생성
    두 모델 admin 페이지 등록
    컬럼별 verbose_name 추가

--- backend/apps/park/admin.py
+++ backend/apps/park/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import Park
+from .models import Equipment, Park, ParkEquipment
 
 
 class ParkAdmin(admin.ModelAdmin):
     list_display = (
+        "id",
         "park_name",
         "gu_id",
         "full_address",
@@ -43,5 +44,19 @@ class ParkAdmin(admin.ModelAdmin):
     list_filter = ("dong_address",)
 
 
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "equipment_name", "equipment_type")
+
+    list_filter = ("equipment_type",)
+
+
+class ParkEquipmentAdmin(admin.ModelAdmin):
+    list_display = ("id", "park_id", "equipment_id", "quantity")
+
+    list_filter = ("park_id", "equipment_id")
+
+
 # Register your models here.
 admin.site.register(Park, ParkAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(ParkEquipment, ParkEquipmentAdmin)
--- backend/apps/park/models.py
+++ backend/apps/park/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 # Create your models here.
 class Park(models.Model):
-    park_name = models.CharField(max_length=50)
-    gu_id = models.IntegerField()
-    full_address = models.TextField()
-    si_address = models.CharField(max_length=50)
-    gu_address = models.CharField(max_length=50)
-    dong_address = models.CharField(max_length=50)
+    park_name = models.CharField(verbose_name="공원이름", max_length=50, unique=True)
+    gu_id = models.IntegerField(verbose_name="구ID")
+    full_address = models.TextField(verbose_name="주소")
+    si_address = models.CharField(verbose_name="시", max_length=50)
+    gu_address = models.CharField(verbose_name="구", max_length=50)
+    dong_address = models.CharField(verbose_name="동", max_length=50)
     latitude = models.DecimalField(
         verbose_name="위도(lat)",
         max_digits=10,
@@ -19,10 +19,39 @@ class Park(models.Model):
         max_digits=11,
         decimal_places=8,
     )
-    park_image = models.ImageField(blank=True)
+    park_image = models.ImageField(verbose_name="공원사진", blank=True)
 
     class Meta:
         ordering = ["park_name"]
 
     def __str__(self):
         return self.park_name
+
+
+class Equipment(models.Model):
+    equipment_name = models.CharField(
+        verbose_name="운동기구 이름", max_length=50, unique=True
+    )
+    equipment_type = models.CharField(verbose_name="운동기구 유형", max_length=50)
+
+    def __str__(self):
+        return self.equipment_name
+
+
+class ParkEquipment(models.Model):
+    park_id = models.ForeignKey(
+        Park,
+        on_delete=models.CASCADE,
+        related_name="park",
+        db_column="park_id",
+    )
+    equipment_id = models.ForeignKey(
+        Equipment,
+        on_delete=models.CASCADE,
+        related_name="equipment",
+        db_column="equipment_id",
+    )
+    quantity = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ["park_id"]
