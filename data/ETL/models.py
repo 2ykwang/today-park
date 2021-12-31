58ec84bc5a49920833a50c53bdecc9eb87f567c7
[3기-1반] 조병민 <bmcho91@naver.com>
2021-12-31 21:18:01 +0900

    ETL base create

---
+++ data/ETL/models.py
@@ -0,0 +1,12 @@
+class Park :
+    def __init__(self, data):
+        pass
+
+class ParkEquipment:
+    def __init__(self, data):
+        pass
+
+class Equipment :
+    def __init__(self, data):
+        pass
+
