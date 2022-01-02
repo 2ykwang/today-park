8377e0d41967fbca615a552732095e59a673a5c3
Yeonggwang <immutable000@gmail.com>
2022-01-03 07:30:00 +0900

    feat: 북마크 모델, 직렬화 클래스 구현

--- backend/apps/bookmark/models.py
+++ backend/apps/bookmark/models.py
@@ -1,3 +1,28 @@
+from apps.core.models import BaseModel
+from apps.park.models import Park
+from apps.user.models import User
 from django.db import models
 
-# Create your models here.
+
+class Bookmark(BaseModel):
+    user_id = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="bookmark_user",
+        db_column="user_id",
+        verbose_name="유저 ID",
+    )
+    park_id = models.ForeignKey(
+        Park,
+        on_delete=models.CASCADE,
+        related_name="bookmark_park",
+        db_column="park_id",
+        verbose_name="공원 ID",
+    )
+
+    class Meta:
+        verbose_name = "북마크"
+        verbose_name_plural = "공원 북마크"
+
+    def __str__(self):
+        return f"{self.user_id.username} -> {self.park_id.park_name}"
