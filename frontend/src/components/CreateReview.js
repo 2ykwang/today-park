be78015034b1c1bcf623ff6f8737626d09890e19
MINYOUNG LEE <>
2022-01-07 15:57:11 +0900

    feat: 리뷰 수정 기능 개발

---
+++ frontend/src/components/CreateReview.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { ReactComponent as StarIcon } from "../image/star.svg";
+import { postReviews, updateReview } from "../actions/index";
+
+export function CreateReview({ parkId, reviewId, type, score, content }) {
+  const [clicked, setClicked] = useState([false, false, false, false, false]);
+  const [rating, setRating] = useState(0);
+  const [reviewContent, setreviewContent] = useState("");
+
+  // console.log(parkId,reviewId,type,)
+
+  // 클릭시 별점 등록
+  function handleStarClick(e, idx) {
+    e.preventDefault();
+    let clickStates = [...clicked];
+    for (let i = 1; i < 6; i++) {
+      if (i <= idx) clickStates[i] = true;
+      else clickStates[i] = false;
+    }
+    setClicked(clickStates);
+    setRating(idx);
+  }
+
+  // 수정 클릭시 원래 있던 리뷰 내용 보이게 하는 로직
+  useEffect(() => {
+    setreviewContent(content);
+    let clickStates = [...clicked];
+    for (let i = 1; i < 6; i++) {
+      if (i <= score) clickStates[i] = true;
+      else clickStates[i] = false;
+    }
+    setClicked(clickStates);
+  }, []);
+
+  // 리뷰 POST 요청
+  async function handleCreateReview() {
+    console.log(parkId, rating, reviewContent);
+    let response = await postReviews(parkId, rating, reviewContent);
+  }
+
+  // 리뷰 PUT 요청
+  async function handleUpdateReview() {
+    let response = await updateReview(parkId, reviewId, rating, reviewContent);
+  }
+
+  return (
+    <>
+      <form className="createReview">
+        <div className="rating">
+          <StarIcon
+            className={"star " + (clicked[1] ? "clicked" : null)}
+            onClick={(e) => handleStarClick(e, 1)}
+            width="24"
+            height="24"
+          />
+          <StarIcon
+            className={"star " + (clicked[2] ? "clicked" : null)}
+            onClick={(e) => handleStarClick(e, 2)}
+            width="24"
+            height="24"
+          />
+          <StarIcon
+            className={"star " + (clicked[3] ? "clicked" : null)}
+            onClick={(e) => handleStarClick(e, 3)}
+            width="24"
+            height="24"
+          />
+          <StarIcon
+            className={"star " + (clicked[4] ? "clicked" : null)}
+            onClick={(e) => handleStarClick(e, 4)}
+            width="24"
+            height="24"
+          />
+          <StarIcon
+            className={"star " + (clicked[5] ? "clicked" : null)}
+            onClick={(e) => handleStarClick(e, 5)}
+            width="24"
+            height="24"
+          />
+        </div>
+        <textarea
+          placeholder="내용을 입력해주세요."
+          value={reviewContent}
+          onChange={(e) => {
+            setreviewContent(e.target.value);
+          }}
+        />
+        <br />
+        <button
+          type="submit"
+          onClick={(e) => {
+            e.preventDefault();
+            if (type === "POST") {
+              handleCreateReview();
+            } else if (type === "PUT") {
+              handleUpdateReview();
+              // window.location.replace("/mypage");
+            }
+          }}
+        >
+          등록하기
+        </button>
+      </form>
+    </>
+  );
+}
--- frontend/src/pages/Mypage.js
+++ frontend/src/pages/Mypage.js
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getLoginData } from "../store/loginSlice";
 import { onlyUserReview } from "../actions/index";
 import { ReactComponent as StarIcon } from "../image/star.svg";
-import { updateReview } from "../actions/index";
+import { CreateReview } from "../components/CreateReview";
 
 const ProfileImage = styled.div`
   background-color: #e0e0e0;
@@ -25,12 +25,10 @@ function Mypage() {
   const [editEmail, setEditEmail] = useState(false);
   const [editUsername, setEditUsername] = useState(false);
   const [editReview, setEditReview] = useState(false);
-  // const [reviewResponse, setReviewResponse]
   const [reviewList, setReviewList] = useState([]);
-  const [clicked, setClicked] = useState([false, false, false, false, false]);
-  const [rating, setRating] = useState(0);
-  const [reviewContent, setreviewContent] = useState("");
+  const [clickedReviewIdx, setClickedReviewIdx] = useState(0);
 
+  // 내가 쓴 리뷰 불러오기
   useEffect(() => {
     async function getReviews() {
       const response = await onlyUserReview();
@@ -40,6 +38,7 @@ function Mypage() {
     getReviews();
   }, []);
 
+  // 수정시, 로그인 정보 수정
   useEffect(() => {
     dispatch(
       getLoginData({ email: email, username: username, password: password })
@@ -57,21 +56,6 @@ function Mypage() {
   }
   function handleEditNickname() {
     setEditUsername(false);
-  }
-  function handleStarClick(e, idx) {
-    e.preventDefault();
-    let clickStates = [...clicked];
-    for (let i = 1; i < 6; i++) {
-      if (i <= idx) clickStates[i] = true;
-      else clickStates[i] = false;
-    }
-    setClicked(clickStates);
-    setRating(idx);
-  }
-  async function handleUpdateReview(e) {
-    e.preventDefault();
-    // let response = await updateReview(id, rating, reviewContent);
-    // console.log(response);
   }
 
   return (
@@ -91,116 +75,112 @@ function Mypage() {
               <p>
                 아이디 :
                 {editEmail ? (
-                  <input type="text" value={email} onChange={handleIdChange} />
+                  <>
+                    <input
+                      type="text"
+                      value={email}
+                      onChange={handleIdChange}
+                    />
+                    <button
+                      className="edit"
+                      onClick={() => {
+                        if (username !== "") handleEditId();
+                      }}
+                    >
+                      확정
+                    </button>
+                  </>
                 ) : (
-                  email
-                )}
-                {editEmail ? (
-                  <button className="edit" onClick={handleEditId}>
-                    확정
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      setEditEmail(true);
-                    }}
-                    className="edit"
-                  >
-                    수정
-                  </button>
+                  <>
+                    {email}
+                    <button
+                      onClick={() => {
+                        setEditEmail(true);
+                      }}
+                      className="edit"
+                    >
+                      수정
+                    </button>
+                  </>
                 )}
               </p>
               <p>
                 닉네임 :
                 {editUsername ? (
-                  <input
-                    type="text"
-                    value={username}
-                    onChange={handleNicknameChange}
-                  />
+                  <>
+                    <input
+                      type="text"
+                      value={username}
+                      onChange={handleNicknameChange}
+                    />
+                    <button
+                      className="edit"
+                      onClick={() => {
+                        if (username !== "") handleEditNickname();
+                      }}
+                    >
+                      확정
+                    </button>
+                  </>
                 ) : (
-                  username
-                )}
-                {editUsername ? (
-                  <button className="edit" onClick={handleEditNickname}>
-                    확정
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      setEditUsername(true);
-                    }}
-                    className="edit"
-                  >
-                    수정
-                  </button>
+                  <>
+                    {username}
+                    <button
+                      onClick={() => {
+                        setEditUsername(true);
+                      }}
+                      className="edit"
+                    >
+                      수정
+                    </button>
+                  </>
                 )}
               </p>
-              <div className="sns">
+              {/* <div className="sns">
                 <h3>소셜 계정 연동하기</h3>
                 <button>네이버</button>
                 <button className="kakao">카카오</button>
-              </div>
+              </div> */}
             </div>
           </div>
           <div className="reviewSide">
-            <h3>내가 방문 했던 산스장</h3>
+            <h3>내가 방문 했던 공원</h3>
             <div className="reviewList">
               {editReview ? (
-                <form className="createReview">
-                  <div className="rating">
-                    <StarIcon
-                      className={"star " + (clicked[1] ? "clicked" : null)}
-                      onClick={(e) => handleStarClick(e, 1)}
-                      width="24"
-                      height="24"
-                    />
-                    <StarIcon
-                      className={"star " + (clicked[2] ? "clicked" : null)}
-                      onClick={(e) => handleStarClick(e, 2)}
-                      width="24"
-                      height="24"
-                    />
-                    <StarIcon
-                      className={"star " + (clicked[3] ? "clicked" : null)}
-                      onClick={(e) => handleStarClick(e, 3)}
-                      width="24"
-                      height="24"
-                    />
-                    <StarIcon
-                      className={"star " + (clicked[4] ? "clicked" : null)}
-                      onClick={(e) => handleStarClick(e, 4)}
-                      width="24"
-                      height="24"
-                    />
-                    <StarIcon
-                      className={"star " + (clicked[5] ? "clicked" : null)}
-                      onClick={(e) => handleStarClick(e, 5)}
-                      width="24"
-                      height="24"
-                    />
-                  </div>
-                  <textarea
-                    placeholder="내용을 입력해주세요."
-                    value={reviewContent}
-                    onChange={(e) => {
-                      setreviewContent(e.target.value);
-                    }}
-                  />
-                  <br />
-                  <button type="submit" onClick={handleUpdateReview}>
-                    등록하기
-                  </button>
-                </form>
+                <CreateReview
+                  parkId={reviewList[clickedReviewIdx].park_id}
+                  reviewId={reviewList[clickedReviewIdx].id}
+                  type={"PUT"}
+                  score={reviewList[clickedReviewIdx].score}
+                  content={reviewList[clickedReviewIdx].content}
+                />
               ) : (
                 reviewList.map((item, index) => {
                   return (
                     <div className="review" key={index}>
-                      <h4>{item.username}</h4>
-                      <p>{item.score}</p>
+                      <h4>
+                        {item.parkname}
+                        <p className="rate">
+                          <StarIcon
+                            width="24"
+                            height="24"
+                            fill="#FFB800"
+                            className="star"
+                          />
+                          {item.score}
+                        </p>
+                      </h4>
+                      <p>{item.created_at.slice(0, 10).split("-").join(".")}</p>
                       <p>{item.content}</p>
-                      <div class="btns">
-                        <button>수정</button>
+                      <div className="btns">
+                        <button
+                          onClick={() => {
+                            setEditReview(true);
+                            setClickedReviewIdx(index);
+                          }}
+                        >
+                          수정
+                        </button>
                         <button>삭제</button>
                       </div>
                     </div>
