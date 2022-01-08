f18893f3836e661fc1e911b2a5c5b89e10bed143
Martina Lee <martinalee0528@gmail.com>
2022-01-09 01:36:51 +0900

    feat: 메인페이지 작업
    white컬러 테마로 변경 및 작업시작

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,8 +6,7 @@ import { BasicLink } from "../components/BasicLink";
 import underScroll from "../image/scrollDown.png";
 import cloud from "../image/cloud3.png";
 import sun from "../image/sun.png";
-import backgroundMountain from "../image/backgroundMountain.png";
-import reasonFirstPic from "../image/main/reason1-pic1.png";
+import Tree from "../image/main/tree.png";
 import { DefaultPieChart } from "../components/DefaultPieChart";
 import { DefaultBarChart } from "../components/DefaultBarChart";
 
@@ -72,31 +71,9 @@ function Home() {
           </div>
         </section>
 
-        {/* <div className="intro">
-          <div className="introBackgroundWrapper">
-            <div className="introBackground1"></div>
-            <div className="introBackground2"></div>
-          </div>
-          <div className="intro-cloud-1">
-            <img src={cloud} alt="cloud1" />
-          </div>
-          <div className="intro-cloud-2">
-            <img src={cloud} alt="cloud2" />
-          </div>
-          <div className="intro-cloud-3">
-            <img src={cloud} alt="cloud3" />
-          </div>
-          <div className="intro-sun">
-            <img src={sun} alt="sun" />
-          </div>
-        </div> */}
-
         <section className="secondPage">
           <div className="reason">
             <div className="reason1">
-              {/* <div className="reason1-background">
-              <img src={backgroundMountain} alt="backgroundMountain" />
-            </div> */}
               <div className="reason1Textwrapper">
                 <div className="reason1Title">
                   <p>
@@ -114,11 +91,11 @@ function Home() {
                 </div>
               </div>
               <div className="reason1-statisticswrapper">
-                {/* <div className="reason1-statistics">
+                {/* <div className="reason1Box">
                 <div className="reason1-statistics-back"></div>
-                <div className="reason1-statistics-contents">
+                <div className="reason1Contents">
                   <p>"성인 46%, 코로나 이후, 체중 증가해"</p>
-                  <div className="reason1-chart1">
+                  <div className="reason1Chart1">
                     <DefaultPieChart
                       data={[
                         { name: "체중이 증가했다", value: 46 },
@@ -130,43 +107,67 @@ function Home() {
                   <p>(대한비만협회)</p>
                 </div>
               </div> */}
-                <div className="reason1-statistics">
+                <div className="reason1Box">
                   <div className="reason1-statistics-back">
-                    <img src={reasonFirstPic} alt="reason first"></img>
-                  </div>
-                  <div className="reason1-statistics-contents">
-                    <p>
-                      "배달앱, 전년비 카드 <br />
-                      사용 건수 증가 업종 1위"
-                    </p>
-                    <div className="reason1Chart2">
-                      <div className="reason1CardRank">
-                        <p>1위 : 배달앱</p>
-                        <p>2위 : 아울렛</p>
-                        <p>3위 : 취미시설</p>
+                    <div className="reason1Contents">
+                      <p>
+                        "배달앱, 전년비 카드 <br />
+                        사용 건수 증가 업종 1위"
+                      </p>
+                      <div className="reason1Chart2">
+                        <div className="reason1CardRank">
+                          <p>1위 : 배달앱</p>
+                          <p>2위 : 아울렛</p>
+                          <p>3위 : 취미시설</p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-                <div className="reason1-statistics">
-                  <div className="reason1-statistics-back"></div>
-                  <div className="reason1-statistics-contents">
-                    <p>
-                      "배달음식 1위 치킨, <br />
-                      평균 2233 kcal↑
-                    </p>
-                    <div className="reason1-chart3">
-                      <DefaultBarChart
-                        data={[
-                          { name: "치킨", value: "2233" },
-                          { name: "하루권장", value: "2500" },
-                        ]}
-                        colors={COLORS}
-                      ></DefaultBarChart>
+                <div className="reason1Box">
+                  <div className="reason1-statistics-back">
+                    <div className="reason1Contents">
+                      <p>
+                        "배달음식 1위 치킨, <br />
+                        평균 2233 kcal↑
+                      </p>
+                      <div className="reason1Chart3">
+                        <DefaultBarChart
+                          data={[
+                            { name: "치킨", value: "2233" },
+                            { name: "하루권장", value: "2500" },
+                          ]}
+                          colors={COLORS}
+                        ></DefaultBarChart>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="thirdPage">
+          <div className="intro">
+            <div className="introBackgroundWrapper">
+              <div className="introBackground1"></div>
+              {/* <div className="introBackground2"></div> */}
+            </div>
+            <div className="intro-cloud-1">
+              <img src={cloud} alt="cloud1" />
+            </div>
+            <div className="intro-cloud-2">
+              <img src={cloud} alt="cloud2" />
+            </div>
+            <div className="intro-cloud-3">
+              <img src={cloud} alt="cloud3" />
+            </div>
+            <div className="intro-sun">
+              <img src={sun} alt="sun" />
+            </div>
+            <div className="tree">
+              <img src={Tree} alt="backgroundMountain" />
             </div>
           </div>
         </section>
