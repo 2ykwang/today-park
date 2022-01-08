d3eba9f0571d06488e4f35aea2bb3070b2dde1a3
Martina Lee <martinalee0528@gmail.com>
2022-01-08 20:59:02 +0900

    feat: 메인페이지 스타일 수정
    1차 공유용 커밋

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 // import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import video from "../video/mainVideo.mp4";
@@ -8,8 +7,9 @@ import underScroll from "../image/scrollDown.png";
 import cloud from "../image/cloud3.png";
 import sun from "../image/sun.png";
 import backgroundMountain from "../image/backgroundMountain.png";
-import { DefualtPieChart } from "../components/DefualtPieChart";
-import { DefualtBarChart } from "../components/DefualtBarChart";
+import reasonFirstPic from "../image/main/reason1-pic1.png";
+import { DefaultPieChart } from "../components/DefaultPieChart";
+import { DefaultBarChart } from "../components/DefaultBarChart";
 
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
@@ -40,19 +40,12 @@ const renderCustomizedLabel = ({
   );
 };
 
-const style = {
-  top: "-3vh",
-  left: "0",
-  lineHeight: "1rem",
-  fontSize: "0.9rem",
-};
-
 function Home() {
   return (
     <>
       <Header />
       <div className="mainContainer">
-        <section className="home">
+        <section className="firstPage">
           <div className="introTextWrapper">
             <div className="introText1">
               <p>포스트 코로나,</p>
@@ -61,14 +54,14 @@ function Home() {
             <div className="introText2">
               <p>"오늘의 공원"이 도와드릴게요:)</p>
             </div>
+            <BasicLink to="/search" className="findPark">
+              공원 찾으러 가기 →
+            </BasicLink>
           </div>
           <div className="video">
             <video muted autoPlay loop>
               <source src={video} type="video/mp4" />
             </video>
-            <BasicLink to="/search" className="findPark">
-              공원 찾으러 가기 →
-            </BasicLink>
           </div>
           <div className="underScroll">
             <img src={underScroll} alt="underScroll" />
@@ -79,7 +72,7 @@ function Home() {
           </div>
         </section>
 
-        <div className="intro">
+        {/* <div className="intro">
           <div className="introBackgroundWrapper">
             <div className="introBackground1"></div>
             <div className="introBackground2"></div>
@@ -96,84 +89,87 @@ function Home() {
           <div className="intro-sun">
             <img src={sun} alt="sun" />
           </div>
-        </div>
-        <div className="reason">
-          <div className="reason1">
-            <div className="reason1-background">
+        </div> */}
+
+        <section className="secondPage">
+          <div className="reason">
+            <div className="reason1">
+              {/* <div className="reason1-background">
               <img src={backgroundMountain} alt="backgroundMountain" />
-            </div>
-            <div className="reason1-textwrapper">
-              <div className="reason1-text1">
-                <p>
-                  혹시 나도 <span>건강 침체기?</span>
-                </p>
+            </div> */}
+              <div className="reason1Textwrapper">
+                <div className="reason1Title">
+                  <p>
+                    혹시 나도 <span>건강 침체기?</span>
+                  </p>
+                </div>
+                <div className="reason1Contents">
+                  <p>
+                    코로나 이후 우리가 자주 먹게된 배달 음식.
+                    <br />
+                    그런데 배달 음식 “한 끼”는 <br />
+                    성인 "하루" 권장 섭취 칼로리(2500kcal)의 <br />
+                    80%에 달한답니다.
+                  </p>
+                </div>
               </div>
-              <div className="reason1-text2">
-                <p>
-                  <span>"확찐자"</span> 많이 들어보셨죠?
-                </p>
-                <p>
-                  배달 음식 수요 증가로 인하여 체중이 증가한 사람이
-                  늘어났습니다.
-                </p>
-                <p>
-                  그 이유는 대부분의 배달음식이 칼로리가 <span>"높기"</span>
-                  때문이죠.
-                </p>
-              </div>
-            </div>
-            <div className="reason1-statisticswrapper">
-              <div className="reason1-statistics">
+              <div className="reason1-statisticswrapper">
+                {/* <div className="reason1-statistics">
                 <div className="reason1-statistics-back"></div>
                 <div className="reason1-statistics-contents">
                   <p>"성인 46%, 코로나 이후, 체중 증가해"</p>
                   <div className="reason1-chart1">
-                    <DefualtPieChart
+                    <DefaultPieChart
                       data={[
                         { name: "체중이 증가했다", value: 46 },
                         { name: "체중이 증가하지 않았다", value: 54 },
                       ]}
                       colors={COLORS}
-                    ></DefualtPieChart>
+                    ></DefaultPieChart>
                   </div>
                   <p>(대한비만협회)</p>
                 </div>
-              </div>
-              <div className="reason1-statistics">
-                <div className="reason1-statistics-back"></div>
-                <div className="reason1-statistics-contents">
-                  <p>"배달앱, 전년비 카드 사용 건수 증가 업종 1위"</p>
-                  <div className="reason1-chart2">
-                    <div className="reason1-card-rank">
-                      <p>1위 : 배달앱</p>
-                      <p>2위 : 아울렛</p>
-                      <p>3위 : 취미시설</p>
-                      <p>·</p>
-                      <p>·</p>
+              </div> */}
+                <div className="reason1-statistics">
+                  <div className="reason1-statistics-back">
+                    <img src={reasonFirstPic} alt="reason first"></img>
+                  </div>
+                  <div className="reason1-statistics-contents">
+                    <p>
+                      "배달앱, 전년비 카드 <br />
+                      사용 건수 증가 업종 1위"
+                    </p>
+                    <div className="reason1Chart2">
+                      <div className="reason1CardRank">
+                        <p>1위 : 배달앱</p>
+                        <p>2위 : 아울렛</p>
+                        <p>3위 : 취미시설</p>
+                      </div>
                     </div>
                   </div>
-                  <p>(삼성카드)</p>
                 </div>
-              </div>
-              <div className="reason1-statistics">
-                <div className="reason1-statistics-back"></div>
-                <div className="reason1-statistics-contents">
-                  <p>"배달음식 1위 치킨, 평균 2233 kcal↑</p>
-                  <div className="reason1-chart3">
-                    <DefualtBarChart
-                      data={[
-                        { name: "치킨", value: "2233" },
-                        { name: "하루권장", value: "2500" },
-                      ]}
-                      colors={COLORS}
-                    ></DefualtBarChart>
+                <div className="reason1-statistics">
+                  <div className="reason1-statistics-back"></div>
+                  <div className="reason1-statistics-contents">
+                    <p>
+                      "배달음식 1위 치킨, <br />
+                      평균 2233 kcal↑
+                    </p>
+                    <div className="reason1-chart3">
+                      <DefaultBarChart
+                        data={[
+                          { name: "치킨", value: "2233" },
+                          { name: "하루권장", value: "2500" },
+                        ]}
+                        colors={COLORS}
+                      ></DefaultBarChart>
+                    </div>
                   </div>
-                  <p>(중앙경제)</p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
     </>
   );
