7f6498d6d32857e47dbf6989fe6e163d487ed260
Yeonggwang <immutable000@gmail.com>
2022-01-10 12:52:12 +0900

    chore: heroku 배포 파일 추가

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn -c ./backend/gunicorn.config.py
--- backend/config/gunicorn.conf.py
+++ backend/config/gunicorn.conf.py
@@ -1,5 +1,8 @@
 import multiprocessing
+import os
+
+is_heroku = os.environ.get("HEROKU_DEPLOY", default=0)
 
 bind = ":8000"
-workers = multiprocessing.cpu_count()
+workers = multiprocessing.cpu_count() if not is_heroku else 6
 wsgi_app = "config.wsgi"
