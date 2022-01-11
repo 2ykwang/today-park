7e7b060e492da3c51403df632f88434b07efca11
Yeonggwang <immutable000@gmail.com>
2022-01-12 02:58:45 +0900

    docs: conf.py 설정 변경
    * 발표자료 추가

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,10 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import os
+import sys
 
+sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = "today-park"
@@ -27,7 +30,7 @@ author = "yeonggwang yang, youngsook lee"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
